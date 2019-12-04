531fadd2f39074db9bfd057e02e1c892fcc32304
joseph <joseph@depop.com>
2019-12-04 17:32:39 +0000

    Error when no addresses are returned by fastly api

--- fastly/data_source_ip_ranges.go
+++ fastly/data_source_ip_ranges.go
@@ -59,6 +59,10 @@ func dataSourceFastlyIPRangesRead(d *schema.ResourceData, meta interface{}) erro
 		return fmt.Errorf("Error parsing result: %s", err)
 	}
 
+	if len(result.Addresses) == 0 {
+		return fmt.Errorf("Error: no addresses returned")
+	}
+
 	sort.Strings(result.Addresses)
 
 	if err := d.Set("cidr_blocks", result.Addresses); err != nil {
